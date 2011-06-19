ea7fba1f6533da191fc06bc06d5911eccd860b83
Max Lapan <max.lapan@gmail.com>
2011-06-19 10:54:20 +0400

    ex2_3

--- CS61A/SICP/c2/ex2_2.rkt
+++ CS61A/SICP/c2/ex2_2.rkt
@@ -31,3 +31,36 @@
         (p2 (end-segment seg)))
     (make-point (avg (x-point p1) (x-point p2))
                 (avg (y-point p1) (y-point p2)))))
+
+;; Segment length along X and Y axises
+(define (x-len-segment seg)
+  (- (x-point (end-segment seg))
+     (x-point (start-segment seg))))
+
+(define (y-len-segment seg)
+  (- (y-point (end-segment seg))
+     (y-point (start-segment seg))))
+
+;; Rectangles. Represented by segment of bottom-left to top-right corners.
+(define (make-rectangle bot-left-pt top-right-pt)
+  (make-segment bot-left-pt top-right-pt))
+
+(define (bot-left-rectangle rect)
+  (start-segment rect))
+
+(define (top-right-rectangle rect)
+  (end-segment rect))
+
+(define (dagonal-rectangle rect)
+  rect)
+
+;; utility routines
+(define (perimeter-rectangle rect)
+  (let ((x-len (x-len-segment (diagonal-rectangle rect)))
+        (y-len (y-len-segment (diagonal-rectangle rect))))
+    (+ (* 2 x-len)
+       (* 2 y-len))))
+
+(define (area-rectangle rect)
+  (* (x-len-segment (diagonal-rectangle rect))
+     (y-len-segment (diagonal-rectangle rect))))
